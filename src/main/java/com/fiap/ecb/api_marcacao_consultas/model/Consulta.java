1c54acb63f3227ece85abd034f3a1a4f38203ed0
Thiago Sardinha <thiago.shiromoto@gmail.com>
2025-08-24 17:02:53 -0300

    Aula 18/08/2025 - Implementação Completa do Sistema de Gerenciamento de Usuários Admin

--- src/main/java/com/fiap/ecb/api_marcacao_consultas/model/Consulta.java
+++ src/main/java/com/fiap/ecb/api_marcacao_consultas/model/Consulta.java
@@ -1,7 +1,9 @@
 package com.fiap.ecb.api_marcacao_consultas.model;
+
 import jakarta.persistence.*;
 import lombok.Data;
 import java.time.LocalDateTime;
+
 @Data
 @Entity
 @Table(name = "consultas")
@@ -9,9 +11,17 @@ public class Consulta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private LocalDateTime dataHora;
     private String especialidade;
+    private String status;
+    private String observacao;
+
     @ManyToOne
     @JoinColumn(name = "usuario_id")
     private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "medico_id")
+    private Usuario medico;
 }
